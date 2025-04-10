9d4732d480b972e0a79d6c55a9791d3edc00f702
KimHS <147032519+jejuKIM99@users.noreply.github.com>
2025-04-10 09:16:32 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -183,8 +183,10 @@ const app = createApp({
     },
     formatDate(dateString) {
       const date = new Date(dateString);
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      return date.toLocaleDateString('en-US', options);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+      const day = String(date.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
     },
     resetSidebannerPosition() {
       const leftSidebanner = document.getElementById('left-sidebanner');
